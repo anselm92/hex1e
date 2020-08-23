1bacd63ba2541dc2c420c69b57599d828dda13a5
Anselm Binninger <anselm92@gmail.com>
2020-08-23 11:30:48 +0200

    Redesign whole page
    - Also add about us and custom pc page

--- webapp/urls.py
+++ webapp/urls.py
@@ -10,12 +10,15 @@ app_name = 'webapp'
 
 urlpatterns = [
 
-  path('', BlogListView.as_view(), name='blog-list'),
-  path('index/<int:month>/<int:year>', BlogListView.as_view(), name='blog-list'),
+  path('blog/', BlogListView.as_view(), name='blog-list'),
+  path('blog/<int:month>/<int:year>', BlogListView.as_view(), name='blog-list'),
 
   path('raffles/', RaffleListView.as_view(), name='raffle-list'),
   path('raffles/<int:raffle_id>/subscribe/', RaffleParticipantView.as_view(), name='raffle-participant-create'),
 
+  path('', TemplateView.as_view(template_name='webapp/index.html'), name='index'),
+  path('about-us/', TemplateView.as_view(template_name='webapp/about-us.html'), name='about-us'),
+  path('custom-pcs/', TemplateView.as_view(template_name='webapp/custom-pcs.html'), name='custom-pcs'),
   path('success/', TemplateView.as_view(template_name='webapp/success.html'), name='success'),
   path('privacy-policy/', TemplateView.as_view(template_name='webapp/privacy-policy.html'), name='privacy-policy'),
   path('impressum/', TemplateView.as_view(template_name='webapp/impressum.html'), name='impressum'),
